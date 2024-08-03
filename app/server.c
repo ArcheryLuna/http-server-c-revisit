3c7de06b81aab7b03725cf5da24b8b4cf766029c
Sebastian Luna <me@archery-luna.com>
2024-08-03 02:33:49 +0100

    codecrafters submit [skip ci]

--- app/server.c
+++ app/server.c
@@ -61,6 +61,41 @@ void handle_client(int client_socket) {
             "\r\n"
             "%s",
             strlen(echo_string), echo_string);
+    } else if (strcmp(path, "/user-agent") == 0) {
+        // Handle /user-agent path
+        char *user_agent = strstr(buffer, "User-Agent: ");
+        if (user_agent != NULL) {
+            user_agent += 12; // Move past "User-Agent: "
+            char *ua_end = strstr(user_agent, "\r\n");
+            if (ua_end != NULL) {
+                int ua_length = ua_end - user_agent;
+                char ua_value[ua_length + 1];
+                strncpy(ua_value, user_agent, ua_length);
+                ua_value[ua_length] = '\0';
+                
+                sprintf(response,
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/plain\r\n"
+                    "Content-Length: %d\r\n"
+                    "\r\n"
+                    "%s",
+                    ua_length, ua_value);
+            } else {
+                sprintf(response, 
+                    "HTTP/1.1 400 Bad Request\r\n"
+                    "Content-Type: text/plain\r\n"
+                    "Content-Length: 15\r\n"
+                    "\r\n"
+                    "400 Bad Request");
+            }
+        } else {
+            sprintf(response, 
+                "HTTP/1.1 400 Bad Request\r\n"
+                "Content-Type: text/plain\r\n"
+                "Content-Length: 15\r\n"
+                "\r\n"
+                "400 Bad Request");
+        }
     } else {
         // Handle other paths
         sprintf(response, 
@@ -76,48 +111,48 @@ void handle_client(int client_socket) {
 }
 
 int main() {
-	// Disable output buffering
-	setbuf(stdout, NULL);
- 	setbuf(stderr, NULL);
+    // Disable output buffering
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    printf("Logs from your program will appear here!\n");
 
-	// Uncomment this block to pass the first stage
-	//
-	int server_fd, client_addr_len, client_socket;
-	struct sockaddr_in client_addr;
-	//
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
-	    printf("Socket creation failed: %s...\n", strerror(errno));
-	    return 1;
-	}
-	//
-	// // Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// // ensures that we don't run into 'Address already in use' errors
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+    // Uncomment this block to pass the first stage
+    //
+    int server_fd, client_addr_len, client_socket;
+    struct sockaddr_in client_addr;
+    //
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        printf("Socket creation failed: %s...\n", strerror(errno));
+        return 1;
+    }
+    //
+    // // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // // ensures that we don't run into 'Address already in use' errors
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
         printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-	    return 1;
-	}
+        return 1;
+    }
 
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-	 								 .sin_port = htons(4221),
-	 								 .sin_addr = { htonl(INADDR_ANY) },
-	 								};
-	
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
-	    printf("Bind failed: %s \n", strerror(errno));
-	    return 1;
-	}
+    struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
+                                     .sin_port = htons(4221),
+                                     .sin_addr = { htonl(INADDR_ANY) },
+                                    };
+    
+    if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+        printf("Bind failed: %s \n", strerror(errno));
+        return 1;
+    }
 
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-	    printf("Listen failed: %s \n", strerror(errno));
-	    return 1;
-	}
-	
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        printf("Listen failed: %s \n", strerror(errno));
+        return 1;
+    }
+    
     while (1) {
         client_socket = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
 
@@ -129,7 +164,8 @@ int main() {
         handle_client(client_socket);
     }
 
-	close(server_fd);
+    close(server_fd);
 
-	return 0;
+    return 0;
 }
+
